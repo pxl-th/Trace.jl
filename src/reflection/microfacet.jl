89acefc22eb570e5f18dbfd242c3fa3f722bf0a3
Anton Smirnov <17990405+pxl-th@users.noreply.github.com>
2021-01-13 19:29:11 +0200

    Add TrowbridgeReitzDistribution

--- src/reflection/microfacet.jl
+++ src/reflection/microfacet.jl
@@ -66,9 +66,9 @@ function λ(trd::TrowbridgeReitzDistribution, w::Vec3f0)::Float32
     θ = w |> tan_θ |> abs
     isinf(θ) && return 0f0
 
-    α = sqrt(cos_ϕ(w) ^ 2 * trd.α_x ^ 2 + sin_ϕ(w) ^ 2 * trd.α_y ^ 2)
-    α²tanθ = (α * θ) ^ 2
-    (-1f0 + sqrt(1f0 + α²tanθ)) / 2f0
+    α = √(cos_ϕ(w) ^ 2 * trd.α_x ^ 2 + sin_ϕ(w) ^ 2 * trd.α_y ^ 2)
+    α²tanθ² = (α * θ) ^ 2
+    (-1f0 + √(1f0 + α²tanθ²)) / 2f0
 end
 
 """
@@ -80,13 +80,121 @@ correspond to near-perfect specular reflection, rather than by specifying
     roughness = max(1f-3, roughness)
     x = log(roughness)
     1.62142f0 + 0.819955f0 * x + 0.1734f0 * x ^ 2 +
-        0.0171201f0 * x ^ 3 + 0.000640711f * x ^ 4
+        0.0171201f0 * x ^ 3 + 0.000640711f0 * x ^ 4
+end
+
+@inline function G1(m::MicrofacetDistribution, w::Vec3f0)::Float32
+    1f0 / (1f0 + λ(m, w))
+end
+
+@inline function G(m::MicrofacetDistribution, wo::Vec3f0, wi::Vec3f0)::Float32
+    1f0 / (1f0 + λ(m, wo) + λ(m, wi))
 end
 
 """
-- G1
-- G
-- D
-- PDF
-- Sample_wh
+Distribution function, which gives the differential area of microfacets
+with the surface normal `w`.
 """
+function D(trd::TrowbridgeReitzDistribution, w::Vec3f0)::Float32
+    tan_θ² = tan_θ(w) ^ 2
+    isinf(tan_θ²) && return 0f0
+
+    cos_θ⁴ = cos_θ(w) ^ 4
+    e = (cos_ϕ(w) ^ 2 / (trd.α_x ^ 2) + sin_ϕ(w) ^ 2 / (trd.α_y ^ 2)) * tan_θ²
+    1f0 / (π * trd.α_x * trd.α_y * cos_θ⁴ * (1f0 + e) ^ 2)
+end
+
+function Pdf(m::MicrofacetDistribution, wo::Vec3f0, wh::Vec3f0)::Float32
+    !m.sample_visible_area && return D(m, wh) * abs(cos_θ(wh))
+    D(m, wh) * G1(m, wo) * abs(wo ⋅ wh) / abs(cos_θ(wo))
+end
+
+function _trowbridge_reitz_sample(
+    cosθ::Float32, u1::Float32, u2::Float32,
+)::Tuple{Float32, Float32}
+    if cosθ > 0.9999f0 # Special case -- normal incidence.
+        r = √(u1 / (1f0 - u1))
+        ϕ = 6.28318530718 * u2
+        return r * cos(ϕ), r * sin(ϕ)
+    end
+
+    sinθ = √(max(0f0, 1f0 - cosθ ^ 2))
+    tanθ = sinθ / cosθ
+    a = 1f0 / tanθ
+    g1 = 2f0 / (1f0 + √(1f0 + 1f0 / (a ^ 2)))
+
+    # Sample slope x.
+    a = 2f0 * u1 / g1 - 1f0
+    tmp = 1f0 / (a ^ 2 - 1f0)
+    tmp > 1f10 && (tmp = 1f10;)
+    b = tanθ
+    b² = b ^ 2
+    d = √(max(0f0, b² * tmp ^ 2 - (a ^ 2 - b²) * tmp))
+    slope_x1, slope_x2 = b * tmp - d, b * tmp + d
+    slope_x = (a < 0 || slope_x2 > 1f0 / tanθ) ? slope_x1 : slope_x2
+
+    # Sample slope y.
+    if u2 > 0.5f0
+        s = 1f0
+        u2 = 2f0 * (u2 - 0.5f0)
+    else
+        s = -1f0
+        u2 = 2f0 * (0.5f0 - u2)
+    end
+    z = (
+        (u2 * (u2 * (u2 * 0.27385f0 - 0.73369f0) + 0.46341f0))
+        / (u2 * (u2 * (u2 * 0.093073f0 + 0.309420f0) - 1f0) + 0.597999f0)
+    )
+    slope_y = s * z * √(1f0 + slope_x ^ 2)
+
+    @assert !isinf(slope_y) && !isnan(slope_y)
+    slope_x, slope_y
+end
+
+function trowbridge_reitz_sample(
+    wi::Vec3f0, α_x::Float32, α_y::Float32, u1::Float32, u2::Float32,
+)::Vec3f0
+    # Stretch wi.
+    wi_stretch = Vec3f0(wi[1] * α_x, wi[2] * α_y, wi[3]) |> normalize
+    slope_x, slope_y = _trowbridge_reitz_sample(cos_θ(wi_stretch), u1, u2)
+    # Rotate.
+    c, s = cos_ϕ(wi_stretch), sin_ϕ(wi_stretch)
+    tmp = c * slope_x - s * slope_y
+    slope_y = s * slope_x + c * slope_y
+    slope_x = tmp
+    # Unstretch.
+    slope_x *= α_x
+    slope_y *= α_y
+    # Compute normal.
+    Vec3f0(-slope_x, -slope_y, 1f0) |> normalize
+end
+
+function sample_wh(
+    trd::TrowbridgeReitzDistribution, wo::Vec3f0, u::Point2f0,
+)::Vec3f0
+    if trd.sample_visible_area
+        flip = wo[3] < 0f0
+        wh = trowbridge_reitz_sample(
+            flip ? -wo : wo, trd.α_x, trd.α_y, u[1], u[2],
+        )
+        return flip ? -wh : wh
+    end
+    cosθ = 0f0
+    ϕ = 2f0 * π * u[2]
+    if trd.α_x ≈ trd.α_y
+        tanθ² = trd.α_x ^ 2 * u[1] / (1f0 - u[1])
+        cosθ = 1f0 / √(1f0 + tanθ²)
+    else
+        ϕ = atan(trd.α_y / trd.α_x * tan(2f0 * π * u[2] + 0.5f0 * π))
+        u[2] > 0.5f0 && (ϕ += π)
+        sinϕ, cosϕ = sin(ϕ), cos(ϕ)
+        α_x², α_y² = trd.α_x ^ 2, trd.α_y ^ 2
+        α² = 1f0 / (cosϕ ^ 2 / α_x² + sinϕ ^ 2 / α_y²)
+        tanθ² = α² * u[1] / (1f0 - u[1])
+        cosθ = 1f0 / √(1f0 + tanθ²)
+    end
+
+    sinθ = √(max(0f0, 1f0 - cosθ ^ 2))
+    wh = spherical_direction(sinθ, cosθ, ϕ)
+    same_hemisphere(wo, wh) ? wh : -wh
+end
