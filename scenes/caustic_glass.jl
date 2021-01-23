a965a5eb62c9d79253891214ac823c4b8d9bd568
Anton Smirnov <anton.smn@protonmail.com>
2021-01-23 19:21:36 +0200

    Add multithreading
    - Add multithreading to WhittedIntegrator
    - Add progress meter

--- scenes/caustic_glass.jl
+++ scenes/caustic_glass.jl
@@ -1,16 +1,23 @@
 using GeometryBasics
 using Trace
+using ProgressMeter
+using Printf
+using FileIO
+using ImageCore
 
 function render()
     model = raw"./scenes/models/caustic-glass.ply"
 
     red = Trace.MatteMaterial(
         Trace.ConstantTexture(Trace.RGBSpectrum(1f0, 0f0, 0f0)),
-        Trace.ConstantTexture(0.5f0),
+        Trace.ConstantTexture(0f0),
     )
     white = Trace.MatteMaterial(
         Trace.ConstantTexture(Trace.RGBSpectrum(1f0, 1f0, 1f0)),
         Trace.ConstantTexture(0f0),
+    )
+    mirror = Trace.MirrorMaterial(
+        Trace.ConstantTexture(Trace.RGBSpectrum(1f0, 1f0, 1f0)),
     )
     glass = Trace.GlassMaterial(
         Trace.ConstantTexture(Trace.RGBSpectrum(1f0)),
@@ -21,13 +28,21 @@ function render()
         true,
     )
 
+    sphere_primitive1 = Trace.GeometricPrimitive(Trace.Sphere(
+        Trace.ShapeCore(Trace.translate(Vec3f0(1.3, 2.2, -98.2)), false),
+        0.2f0, 360f0,
+    ), red)
+    sphere_primitive2 = Trace.GeometricPrimitive(Trace.Sphere(
+        Trace.ShapeCore(Trace.translate(Vec3f0(2, 0.26, -96)), false),
+        0.25f0, 360f0,
+    ), red)
+
     transformation = Trace.translate(Vec3f0(5, -1.49, -100))
     core = Trace.ShapeCore(transformation, false)
     triangle_meshes, triangles = Trace.load_triangle_mesh(core, model)
-    triangle_primitives = [Trace.GeometricPrimitive(t, glass) for t in triangles]
 
     floor_triangles = Trace.create_triangle_mesh(
-        Trace.ShapeCore(Trace.translate(Vec3f0(-2, 0, -86)), false),
+        Trace.ShapeCore(Trace.translate(Vec3f0(-2, 0, -87)), false),
         4,
         UInt32[
             1, 2, 3,
@@ -47,39 +62,64 @@ function render()
             Trace.Normal3f0(0, 0, 1), Trace.Normal3f0(0, 0, 1),
         ],
     )
-    for t in floor_triangles
-        push!(triangle_primitives, Trace.GeometricPrimitive(t, white))
-    end
 
-    bvh = Trace.BVHAccel(triangle_primitives, 1)
+    n_primitives = length(triangles) + length(floor_triangles) + 2
+    primitives = Vector{Trace.GeometricPrimitive}(undef, n_primitives)
+    filled = 1
+
+    primitives[filled] = sphere_primitive1
+    filled += 1
+    primitives[filled] = sphere_primitive2
+    filled += 1
+    for t in triangles
+        primitives[filled] = Trace.GeometricPrimitive(t, glass)
+        filled += 1
+    end
+    for t in floor_triangles[1:4]
+        primitives[filled] = Trace.GeometricPrimitive(t, white)
+        filled += 1
+    end
+    # for t in floor_triangles[3:4]
+    #     primitives[filled] = Trace.GeometricPrimitive(t, glass)
+    #     filled += 1
+    # end
+    @assert filled == n_primitives + 1
+
+    bvh = Trace.BVHAccel(primitives, 1)
     println("BVH World bounds $(Trace.world_bound(bvh))")
 
+    intensity = 150f0 * π
     lights = [Trace.PointLight(
-        Trace.translate(Vec3f0(-5, 10, -70)),
-        Trace.RGBSpectrum(Float32(680 * π)),
+        Trace.translate(Vec3f0(-5, 10, -90)),
+        Trace.RGBSpectrum(intensity),
     )]
     scene = Trace.Scene(lights, bvh)
 
-    resolution = Point2f0(1024)
-    filter = Trace.LanczosSincFilter(Point2f0(1f0), 3f0)
-    film = Trace.Film(
-        resolution, Trace.Bounds2(Point2f0(0f0), Point2f0(1f0)),
-        filter, 1f0, 1f0,
-        "scenes/caustic-glass-$(Int64(resolution[1]))x$(Int64(resolution[2])).png",
-    )
-    screen = Trace.Bounds2(Point2f0(-1f0), Point2f0(1f0))
+    resolution = Point2f0(256)
+    n_samples = 8
+    ray_depth = 8
+
     # look_point = Trace.bounding_sphere(Trace.world_bound(bvh))[1]
     # look_point *= Point3f0(0, 0, 1)
     # look_point -= Point3f0(0.5, 2, 0)
     look_point = Point3f0(-0.5, -2.0, -101.750374)
     println("Look point $look_point")
+
+    screen = Trace.Bounds2(Point2f0(-1f0), Point2f0(1f0))
+    filter = Trace.LanczosSincFilter(Point2f0(1f0), 3f0)
+
+    ir = resolution .|> Int64
+    film = Trace.Film(
+        resolution, Trace.Bounds2(Point2f0(0), Point2f0(1)),
+        filter, 1f0, 1f0, "./scenes/caustic-$(ir[1])x$(ir[2]).png",
+    )
     camera = Trace.PerspectiveCamera(
-        Trace.look_at(Point3f0(0, 50, 100), look_point, Vec3f0(0, 1, 0)),
+        Trace.look_at(Point3f0(0, 60, 100), look_point, Vec3f0(0, 1, 0)),
         screen, 0f0, 1f0, 0f0, 1f6, 90f0, film,
     )
 
-    sampler = Trace.UniformSampler(8)
-    integrator = Trace.WhittedIntegrator(camera, sampler, 8)
+    sampler = Trace.UniformSampler(n_samples)
+    integrator = Trace.WhittedIntegrator(camera, sampler, ray_depth)
     scene |> integrator
 end
 
